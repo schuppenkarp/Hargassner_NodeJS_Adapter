d2805a6f3467cb85b0381d78d7f8917ef6f9718d
PaulOberm <37973902+PaulOberm@users.noreply.github.com>
2025-03-23 10:02:06 +0100

    Adds timestamp and site to data in hgboiler.js

--- bin/hgboiler.js
+++ bin/hgboiler.js
@@ -8,9 +8,9 @@ const options = getopt({
   raw: { description: 'emit raw format instead of JSON' },
   model: { description: 'model name', args: 1, required: false, default: 'default' },
   once: { description: 'emit one reading and exit' },
-  timestamps: { description: 'Log timestamp in returned data', required: false },
-  site: { description: 'Name of site where Hargassner is deployed', required: false},
-  endpoint: { description: 'Send data to public HTTP/HTTPS endpoint', required: false }
+  timestamps: { description: 'Log timestamp in returned data', default: false, required: false },
+  site: { description: 'Name of site where Hargassner is deployed', default: '', required: false},
+  endpoint: { description: 'Send data to public HTTP/HTTPS endpoint', default: '', required: false }
 })
 
 const heizung = new HargassnerTelnet({ IP: options.ip, model: options.model })
@@ -20,10 +20,12 @@ heizung.connect({ quiet: 1 })
 heizung.on('data', data => {
   if (options.timestamps) {
     // Add local timestamp to data
+    data['timestamp'] = Date.now()
   }
 
   if (options.site) {
     // Add local site name to data
+    data['site'] = options.site
   }
 
   if (options.raw) {
