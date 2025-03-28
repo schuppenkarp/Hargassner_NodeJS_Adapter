179757262de81c1d3aaeb5f1814192c53580d79b
Pabloberm <paul.obermayer92@gmail.com>
2025-03-28 23:36:41 +0100

    Use global node interpreter

--- test/hgboiler-cli-test.bash
+++ test/hgboiler-cli-test.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/bin/node -e "require('./test/mockserver.js').start(1025, () => console.log('Mock server running on port 1025'))" & MOCK_SERVER_PID=$!
+node -e "require('./test/mockserver.js').start(1025, () => console.log('Mock server running on port 1025'))" & MOCK_SERVER_PID=$!
 sleep 2
 
 echo "Running tests..."
-/usr/bin/node ./bin/hgboiler.js --ip 0.0.0.0 --port 1025 --once true
+node ./bin/hgboiler.js --ip 0.0.0.0 --port 1025 --once true
 
 echo "Stopping mock server..."
 kill $MOCK_SERVER_PID
