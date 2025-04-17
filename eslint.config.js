67d392c14a940b07503bdd48ca8fc11e9882f705
Pabloberm <paul.obermayer92@gmail.com>
2025-04-17 23:23:53 +0200

    fix: debug eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,6 @@
 const { defineConfig } = require("eslint/config");
 
 module.exports = defineConfig({
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
   parserOptions: {
     ecmaVersion: 12,
   },
